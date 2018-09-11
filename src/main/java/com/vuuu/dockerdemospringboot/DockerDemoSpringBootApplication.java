1345eb53deb186eb7859abea93f5ac394777a499
ryanicer <40254423+ryanicer@users.noreply.github.com>
2018-09-11 22:48:50 +0800

    init project

---
+++ src/main/java/com/vuuu/dockerdemospringboot/DockerDemoSpringBootApplication.java
@@ -0,0 +1,18 @@
+package com.vuuu.dockerdemospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@RestController
+@SpringBootApplication
+public class DockerDemoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoSpringBootApplication.class, args);
+	}
+
+	@RequestMapping("")
+    public String hello(){
+        return "Hello! Docker!";
+    }
+}
---
+++ src/test/java/com/vuuu/dockerdemospringboot/DockerDemoSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vuuu.dockerdemospringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerDemoSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
